2b92fae09dd745cce2e3fd05769707dd8679479a
chris <chris@new-host.home>
2009-07-16 20:56:42 -0400

    Reworked Icon set and cleaned up extraneous files. Icons from the tango project: http://tango.freedesktop.org/Tango_Icon_Library

--- install.js
+++
@@ -1,7 +0,0 @@
-//const VERSION		= "0.4";
-//const KEY			= "/clearcache";
-
-// Defaults directory and preferences
-//addDirectory(KEY, VERSION, "defaults", defaultsDir, null);
-//defaultsDir = getFolder(getFolder("Program", "defaults"), "pref");
-//addFile(KEY, VERSION, "defaults/preferences/prefs.js", defaultsDir, null);
