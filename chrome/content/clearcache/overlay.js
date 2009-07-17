775514c7cd59472eb2c7841612eff9d76715686a
chris <chris@new-host.home>
2009-07-16 23:09:00 -0400

    manually merged in changes from Gabriel Mariani. Changes adds a notification that the cache has been cleared.

--- chrome/content/clearcache/overlay.js
+++ chrome/content/clearcache/overlay.js
@@ -1,39 +1,37 @@
 //
 // Clear the cache
 //
-
 function clearcachebutton_clearCache()
 {
-    var cacheService = getCacheService();
+    var cacheService     = getCacheService();
+    var alertService     = Components.classes["@mozilla.org/alerts-service;1"].getService(Components.interfaces.nsIAlertsService);
 
     try
     {
         cacheService.evictEntries(Components.interfaces.nsICache.STORE_ON_DISK);
         cacheService.evictEntries(Components.interfaces.nsICache.STORE_IN_MEMORY);
+        notify("Cache Cleared", "Cache has been cleared");
     }
     catch(exception)
     {
-        alert(exception.description);
+        notify("Exception", exception);
     }
-}
 
-//
-// return a reference to the cache service
-//
-function getCacheService()
-{
-    var cacheClass = Components.classes["@mozilla.org/network/cache-service;1"];
-    var service    = cacheClass.getService(Components.interfaces.nsICacheService);
+    //
+    // return a reference to the cache service
+    //
+    function getCacheService()
+    {
+        var cacheClass = Components.classes["@mozilla.org/network/cache-service;1"];
+        var service    = cacheClass.getService(Components.interfaces.nsICacheService);
 
-    return service;
-}
+        return service;
+    }
 
-//
-// Work in progress. not in use
-//
-function VersionChecker()
-{
-    var pref = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-    var x = pref.getCharPref("extensions.{563e4790-7e70-11da-a72b-0800200c9a66}.description");
-    alert(x);
+    // Notify user
+    function notify(title, body)
+    {
+        alertService.showAlertNotification("chrome://clearcache/skin/logo_32.png", "Clear Cache", body, false, "", null);
+    }
+
 }
