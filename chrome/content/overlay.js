411e83ac21da51dd67c3bff8106d7eb3b5f61bbd
choltz <choltz@loki.(none)>
2010-03-28 23:11:35 -0400

    incremental updates so extension functions don't live in the global namespace.

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,3 +1,68 @@
+if(!com) var com={};
+if(!com.chrisholtz) com.chrisholtz={};
+
+com.chrisholtz.clearcache = function()
+{
+    var public  = {};
+    var private = {};
+
+    private.alertService = Components.classes["@mozilla.org/alerts-service;1"].getService(Components.interfaces.nsIAlertsService);
+    private.preferences  = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+    private.cacheService = Components.classes["@mozilla.org/network/cache-service;1"].getService(Components.interfaces.nsICacheService);
+    
+    private.notify = function(title, body)
+    {
+        try
+        {
+            private.alertService.showAlertNotification("chrome://clearcache/skin/logo_32.png", title, body, false, "", null);
+        }
+        catch(exception)
+        {
+            // don't do anything if notification services aren't set up on the computer
+        }
+    }
+
+    public.clearcache = function()
+    {
+        try
+        {
+            private.cacheService.evictEntries(Components.interfaces.nsICache.STORE_ON_DISK);
+            private.cacheService.evictEntries(Components.interfaces.nsICache.STORE_IN_MEMORY);
+
+            //var loc          = new localizedStrings();
+            //alert(loc.popupTitle);
+
+            // show the notification if the user hasn't yet set the
+            // preference or they've explicitly enabled it
+            try
+            {
+                if (private.preferences.getBoolPref("extension.clearcache.showNotification") == true)
+                {
+                    private.notify(loc.popupTitle, loc.successMessage);
+                }
+            }
+            catch(exception)
+            {
+                // if there's an error it's likely because the preference value
+                // hasn't been set yet. If that's the case then show the
+                // preferences dialog
+                openDialog("chrome://clearcache/content/options.xul");
+            }
+
+        }
+        catch(e)
+        {
+            private.notify("Error", e.description);
+        }
+    };
+
+    return public;
+}();
+
+com.chrisholtz.clearcache.clearcache();
+
+
+
 //
 // Clear the cache
 //
