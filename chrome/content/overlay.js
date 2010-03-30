c8ae9125d47da993ff3d4722fc464c3ddc06c6fa
choltz <choltz@loki.(none)>
2010-03-29 22:28:10 -0400

    More incremental changes to encapsulate clearcache behavior into its own namespace.

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,6 +1,45 @@
 if(!com) var com={};
 if(!com.chrisholtz) com.chrisholtz={};
 
+//
+// Encapsulate localized string functionality in its own object.
+// Include fail-safes if local strings are not found
+//
+com.chrisholtz.localizedStrings = function()
+{
+    var public  = {};
+    var private = {};
+
+    // get the specificed string from the cache bundle. Trap error if property is not found
+    private.getString = function(key)
+    {
+        var loc   = document.getElementById("cachebundle");
+        var value = "";
+
+        try
+        {
+            value = loc.getString("extensions.{563e4790-7e70-11da-a72b-0800200c9a66}." + key);
+        }
+        catch(e) {} // If there is trouble retrieving the localized string, then we'll assign defaults
+
+        // assign defaults if there was a problem retrieving the string
+        if (value == "")
+        {
+            value = (key == "popupTitle" ? "Clear Cache" : "The cache has been cleared");
+        }
+
+        return value;
+    }
+
+    public.popupTitle     = private.getString("popupTitle");
+    public.successMessage = private.getString("popupSuccess");
+
+    return public
+}();
+
+//
+// Object to encapsulate clear cache behavior
+//
 com.chrisholtz.clearcache = function()
 {
     var public  = {};
@@ -10,6 +49,7 @@ com.chrisholtz.clearcache = function()
     private.preferences  = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
     private.cacheService = Components.classes["@mozilla.org/network/cache-service;1"].getService(Components.interfaces.nsICacheService);
     
+    // popup notification to alert user when the cache is cleared
     private.notify = function(title, body)
     {
         try
@@ -22,15 +62,16 @@ com.chrisholtz.clearcache = function()
         }
     }
 
+    // clear the browser cache
     public.clearcache = function()
     {
         try
         {
+            // clear memory and disk cache
             private.cacheService.evictEntries(Components.interfaces.nsICache.STORE_ON_DISK);
             private.cacheService.evictEntries(Components.interfaces.nsICache.STORE_IN_MEMORY);
 
-            //var loc          = new localizedStrings();
-            //alert(loc.popupTitle);
+            var loc = com.chrisholtz.localizedStrings;
 
             // show the notification if the user hasn't yet set the
             // preference or they've explicitly enabled it
@@ -58,103 +99,3 @@ com.chrisholtz.clearcache = function()
 
     return public;
 }();
-
-com.chrisholtz.clearcache.clearcache();
-
-
-
-//
-// Clear the cache
-//
-function clearcachebutton_clearcache()
-{
-    try
-    {
-        var cacheService = getCacheService();
-        var alertService = Components.classes["@mozilla.org/alerts-service;1"].getService(Components.interfaces.nsIAlertsService);
-        var prefs        = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-        var loc          = new localizedStrings();
-
-        cacheService.evictEntries(Components.interfaces.nsICache.STORE_ON_DISK);
-        cacheService.evictEntries(Components.interfaces.nsICache.STORE_IN_MEMORY);
-
-        // show the notification if the user hasn't yet set the
-        // preference or they've explicitly enabled it
-        try
-        {
-          showNotification = prefs.getBoolPref("extension.clearcache.showNotification");
-
-          if (showNotification == true)
-          {
-            notify(loc.popupTitle, loc.successMessage);
-          }
-        }
-        catch(exception)
-        {
-            // if there's an error it's likely because the preference value
-            // hasn't been set yet. If that's the case then show the
-            // preferences dialog
-            openDialog("chrome://clearcache/content/options.xul");
-        }
-    }
-    catch(e)
-    {
-        notify("Error", e.description);
-    }
-
-    //
-    // return a reference to the cache service
-    //
-    function getCacheService()
-    {
-        var cacheClass = Components.classes["@mozilla.org/network/cache-service;1"];
-        var service    = cacheClass.getService(Components.interfaces.nsICacheService);
-
-        return service;
-    }
-
-    // Notify user
-    function notify(title, body)
-    {
-        try
-        {
-            alertService.showAlertNotification("chrome://clearcache/skin/logo_32.png", title, body, false, "", null);
-        }
-        catch(exception)
-        {
-            // don't do anything if notification services aren't set up on the computer
-        }
-    }
-}
-
-//
-// Encapsulate localized string functionality in its own object.
-// Include fail-safes if local strings are not found
-//
-function localizedStrings()
-{
-    var loc                                   = document.getElementById("cachebundle");
-    localizedStrings.prototype.popupTitle     = getString("popupTitle");
-    localizedStrings.prototype.successMessage = getString("popupSuccess");
-
-    // get the specificed string from the cache bundle. Trap error if property is not found
-    function getString(key)
-    {
-        var value = "";
-
-        try
-        {
-            value = loc.getString("extensions.{563e4790-7e70-11da-a72b-0800200c9a66}." + key);
-        }
-        catch(e) {} // If there is trouble retrieving the localized string, then we'll assign defaults
-
-        // assign defaults if there was a problem retrieving the string
-        if (value == "")
-        {
-            value = (key == "popupTitle" ? "Clear Cache" : "The cache has been cleared");
-        }
-
-        return value;
-    }
-}
-
